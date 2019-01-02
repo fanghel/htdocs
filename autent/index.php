dcc2e739472283bdb5bbd66f1c8ac9f5dfce60be
Florin Anghel <florin.anghel@ugal.ro>
2019-01-02 12:52:10 +0200

    Optimize website
    Signed-off-by: Florin Anghel <florin.anghel@ugal.ro>

--- autent/index.php
+++ autent/index.php
@@ -1,5 +1,5 @@
 <?php
-   // session_start();
+   session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,38 +8,41 @@
     <title>Document</title>
 </head>
 <body>
-    <div class="header">
-        <div class="container">
-            <div class="logo">
-                <img src="https://picsum.photos/200/150"
-                    width="100" 
-                    height="100">
-            </div>
-            <div class="nav">
-                <ul>
-                    <li><a href="index.php">HOME</a></li>
-                    <li><a href="signup.php"> SIGN UP</a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
+    <img src="https://picsum.photos/200/150"
+        width="200"
+        height="150">
+        <ul>
+            <li><a href="index.php">HOME</a></li>
+            <?php
+                if (!isset($_SESSION['id'])) {
+                     echo '<li><a href="signup.php"> SIGN UP</a></li>';
+                }
+            ?>
+         </ul>
+
    <?php
-   // if (isset($_SESSION['id'])) {
-    //    echo 'Wellcome'.$_SESSION['prenume'];  
-   // }
-    ?>
-    
-    <h1>Welcome!</h1>   
-    <form method="POST" action="includes/login.inc.php">
-        <input type="text" name="username" placeholder="Username"><br><br>
-        <input type="password" name="password" placeholder="Password"><br><br>
-        <input type="submit" value="Log in">
-    <form>
-    
-    <?php
+     if (!isset($_SESSION['id'])) {
+        echo '
+            <h2>WELCOME<h2>
+            <form method="POST" action="includes/login.inc.php">
+                <input type="text" name="username" placeholder="Username"><br><br>
+                <input type="password" name="password" placeholder="Password"><br><br>
+                <input type="submit" value="Log in">
+            <form>
+        ';
+
         if (isset($_GET['info']) && $_GET['info'] == 'LOGIN_FAILED') {
             echo 'Connection error!';
         }
+
+     } else {
+        echo 'Welcome  '.$_SESSION['surname'];
+        echo'
+            <form action="includes/logout.inc.php"><br><br>
+                <input type="submit" value="Log Out">
+            </form>
+        ';
+     }
     ?>
 </body>
 </html>
--- autent/signup.php
+++ autent/signup.php
@@ -6,20 +6,20 @@
 </head>
 <body>
     <img src="https://picsum.photos/200/150"
-         width="100" 
-         height="100">
+         width="200"
+         height="140">
         <ul>
             <li><a href="index.php">HOME</a></li>
             <li><a href="signup.php">SIGN UP</a></li>
         </ul>
 
-    <h1>Sign up!</h1> 
+    <h1>Sign up!</h1>
     <form method="POST" action="includes/signup.inc.php">
         <input type="text" name="name" placeholder="Name"><br><br>
         <input type="text" name="surname" placeholder="Surname"><br><br>
         <input type="text" name="username" placeholder="Username"><br><br>
         <input type="password" name="password" placeholder="Password"><br><br>
-        <input type="submit" value="Sign up"> <br><br>  
+        <input type="submit" value="Sign up"> <br><br>
     <form>
 
     <?php
@@ -27,13 +27,13 @@
         $status = $_GET['info'];
     else
         return;
-    
+
     $message = [
         'OK'                  => "The account has been created!",
         'ERROR'               => "Please fill in all fields!",
         'ACCOUNT_UNAVAILABLE' => "The username is not available!"
     ];
-    
+
     echo "<p style='text-align:center;'>$message[$status]</p/>";
     echo "<br/>";
     ?>
