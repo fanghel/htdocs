45ca7f43dadb07f783c82fc15119eb3f4722bd16
Florin Anghel <florin.anghel@ugal.ro>
2019-01-02 12:36:01 +0200

    Remove debugging info
    Signed-off-by: Florin Anghel <florin.anghel@ugal.ro>

--- autent/includes/login.inc.php
+++ autent/includes/login.inc.php
@@ -1,13 +1,12 @@
 <?php
-//session_start();
+session_start();
 require 'connection.php';
 
 if (!empty($_POST['username']) && !empty($_POST['password']) &&  isset($_POST['username']) && isset($_POST['password'])) { 
+
     $username = strtolower($_POST['username']);
     $password = $_POST['password'];
-    echo "$username, $password";
-    echo "<br/>";
-    
+        
     $sql = "SELECT * FROM users WHERE username='$username'";
     $result = mysqli_query($conectare, $sql);
     $row = $result->fetch_assoc();
@@ -15,19 +14,15 @@ if (!empty($_POST['username']) && !empty($_POST['password']) &&  isset($_POST['u
     
     $passwordMatches = password_verify($password, $hash);
     if (!$passwordMatches) {
-        // header("Location: ../index.php?info=LOGIN_FAILED");
-        echo "The password is wrong!";
-        echo "<br/>";
+        header("Location: ../index.php?info=LOGIN_FAILED");
         die();
+    } else {
+    	$_SESSION['id'] = $row['id'];
+  		$_SESSION['name'] = $row['name'];
+        $_SESSION['surname'] = $row['surname'];
+        $_SESSION['username'] = $row['username'];
+       
     }
-    
-    echo "Login successful!";
-    echo "<br/>";
 
-    $_SESSION['id'] = $row['id'];
-    $_SESSION['name'] = $row['name'];
-    $_SESSION['surname'] = $row['surname'];
-    $_SESSION['username'] = $row['username'];
-
-    // header("Location: ../index.php");
+    header("Location: ../index.php");
 }
