45cb5f2d475efd577a63e46b45622ab01ed32bc3
Florin Anghel <florin.anghel@ugal.ro>
2019-01-05 11:48:39 +0200

    PHP cu HTML si Java script

---
+++ Tutorial/Index.php
@@ -0,0 +1,16 @@
+<?php
+  echo 'Introducem un text <h1>HTML<h1>';
+  echo '<html>
+<head><title>PHP cu JavaScript</title>
+<script language="JavaScript">
+function alertare () {
+ alert("Acesta este un test PHP ce contine JavaScript")
+}
+</script>
+</head> 
+<body onLoad="alertare()">
+<p>Un script PHP ce contine JavaScript</p>
+</body>
+</html>'; 
+  
+ ?>
