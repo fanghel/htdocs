6093eaa5f37453de190b6240694baad13de21fcf
Florin Anghel <florin.anghel@ugal.ro>
2019-01-11 18:49:26 +0200

    Try things
    Signed-off-by: Florin Anghel <florin.anghel@ugal.ro>

--- Tutorial/Index.php
+++ Tutorial/Index.php
@@ -40,12 +40,97 @@
     Normally, you should keep the html code clean. When you
     need access to some data from php you should use these
     short tags to get it.
-
     The standard way to do these things is to use an MVC
     framework, BUT in order to keep things as simple as
     possible during learning we'll use this approach.
   -->
   <p>The student is <?= $student ?>.</p>
   <p>My name is <?= "$forename $surname" ?> and I'm a student.</p>
+
+<?php
+  $alfa = 12.5;
+  $n = 2;
+  echo $n++ ."\n";
+  echo $n."\n";
+  echo ++$n ;
+  $sgl_quotes = 'String';
+  $dbl_quotes = "This is a $sgl_quotes.";
+  echo $dbl_quotes;
+  $apples = "I have {$n} apples";
+  echo $apples;
+  $oranges = "I have ${n} oranges to eat.";
+  echo $oranges;
+  $money = "I have $${n} in the bank.";
+  echo $money;
+  echo 'This string ' . 'is concatenated.'. "\n";
+
+  define ("pi" , "3.14");
+  echo 'Valoarea lui Pi este ' . pi ."\n";
+
+  $array = ['One' => 1, 'Two' => 2];
+  echo $array['One'] ."\n";
+  $array1 = ['alfa' , 'beta' , 'gama'];
+  echo $array1[1];
+
+  $x = 1;
+  $y = 2;
+  $x = $y;
+  $z = &$y;
+  echo $x; // => 2
+  echo $z; // => 2
+  $z = 7;
+  echo $x; // => 2
+  echo $y; // => 0
+
+  var_dump($z);
+  print_r($array);
+  print_r($array1);
+
+  echo 'Hello';
+  echo 1010 ."\n";
+
+  $a = 100;
+  $b = 1000;
+  echo $a <=> $a; // 0 since they are equal
+  echo $a <=> $b; // -1 since $a < $b
+  echo $b <=> $a ."\n"; // 1 since $b > $a 
+
+  $integer = 58;
+  $string = strval($integer);
+  echo $string;
+  var_dump($string);
+
+  $nume = 'Jane';
+?>
+
+  <p>The name of my friend is <?= $nume ?></p>
+
+<?php
+  $x = 0;
+  echo ($x ? 'Does not get printed.' : 'Does.') ."\n";
+
+  $a = null;
+  $b = '  Scrie ce vrei';
+  echo $a ?? '  a is not set'; // prints 'a is not set'
+  echo $b ?? '  b is not set'; // prints 'Does print'
+  ?>
+
+<?php if ($x): ?>
+<p>This is displayed if the test is truthy.<p>
+<?php else: ?>
+<p>This is displayed otherwise.<p>
+<?php endif; ?>
+
+<?php
+  $wheels = ['bicycle' => 2, 'car' => 4];
+
+  // Foreach loops can iterate over arrays
+  foreach ($wheels as $wheel_count) {
+      echo $wheel_count;
+
+
+
+
+?>
 </body>
 </html>
